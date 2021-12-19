b8aaaf1dd27847705fa54cf0266e28e26db6bb6c
Frederik Nørregaard <frederik@norregaard.info>
2021-12-19 20:57:43 +0100

    fjernet javascript

--- javascript/mobile.js
+++
@@ -1,9 +0,0 @@
-// A function that either adds or remove a class name
-// to an element with the id #menu
-function toggleMobile()
-{
-  var menu = document.getElementById("menu");
-  var hasAnyClass = (menu.className !== "")
-  var nextClass = (hasAnyClass ? "" : "show");
-  menu.className = nextClass;
-}
